9f337360c427183edcdfb5a530739158cc748f07
ShuaibKassim <126048278+Kassim-gif@users.noreply.github.com>
2024-02-11 07:46:55 +0300

    Add files via upload

---
+++ ImageSlideshow-master/app/src/main/java/com/denzcoskun/imageslideshow/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.denzcoskun.imageslideshow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ActionTypes
+import com.denzcoskun.imageslider.constants.AnimationTypes
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemChangeListener
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.interfaces.TouchListener
+import com.denzcoskun.imageslider.models.SlideModel
+
+/**
+ * Created by Deniz Coşkun on 6/23/2020.
+ * denzcoskun@hotmail.com
+ * İstanbul
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider) // init imageSlider
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+        imageList.add(SlideModel("https://t.ly/aaa_", "The future is our hands."))
+        imageList.add(SlideModel("https://t.ly/sg91", "Climate change is moving very fast."))
+        imageList.add(SlideModel("https://t.ly/hqW3", "The population has decreased by 27 percent in the last 5 years."))
+
+        imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP)
+
+        imageSlider.setSlideAnimation(AnimationTypes.ZOOM_OUT)
+
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                // You can listen here.
+                println("normal")
+            }
+
+            override fun doubleClick(position: Int) {
+                // Do not use onItemSelected if you are using a double click listener at the same time.
+                // Its just added for specific cases.
+                // Listen for clicks under 250 milliseconds.
+                println("its double")
+            }
+        })
+
+        imageSlider.setItemChangeListener(object : ItemChangeListener {
+            override fun onItemChanged(position: Int) {
+                //println("Pos: " + position)
+            }
+        })
+
+        imageSlider.setTouchListener(object : TouchListener {
+            override fun onTouched(touched: ActionTypes, position: Int) {
+                if (touched == ActionTypes.DOWN){
+                    imageSlider.stopSliding()
+                } else if (touched == ActionTypes.UP ) {
+                    imageSlider.startSliding(1000)
+                }
+            }
+        })
+    }
+
+}
